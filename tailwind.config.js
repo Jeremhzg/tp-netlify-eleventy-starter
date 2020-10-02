3f0eec1d3b18bf3a66c32c87d6c29e75cf263b29
jeremie herzog <jeremie.herzog@edu.univ-fcomte.fr>
2020-10-02 17:11:27 +0200

    media query tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,19 @@ module.exports = {
     extend: {
       colors:{
         'regal-blue' : '#243c5a'
+      },
+      screens: {
+        'sm': '640px',
+        // => @media (min-width: 640px) { ... }
+
+        'md': '768px',
+        // => @media (min-width: 768px) { ... }
+
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
       }
     },
   },
